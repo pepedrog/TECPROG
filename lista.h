d37984644f3a68a56a2c051abfc5a2cdfc4297ac
pepedrog <gigeck.pedro@hotmail.com>
2018-09-26 19:55:31 -0300

    Busca, insere e retira funfando, só falta o destroi

--- lista.h
+++ lista.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "elemento.h"
 
+//Implementação conforme a sugestão
 typedef struct elo {
 	struct elo * next ;
 	Elemento * val ;
@@ -11,53 +12,70 @@ typedef struct {
 	Elo * cabec ;
 } Lista ;
 
+
 Lista Lcria(void){
+	//Instanciando uma lista e alocando o primeiro endereço (cabec)
 	Lista s;
 	s.cabec = malloc(sizeof(Elo));
+	//Definindo o próximo elo vazio (fim da lista)
 	s.cabec->next = s.cabec->val = NULL;
 	return s;
 }
 
+int stringsIguais(char* s1, char* s2)
+{
+	int i;
+	for(i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++);
+	if(s1[i] == '\0' && s2[i] == '\0') return 1;
+	return 0;
+}
+
 void Ldestroi(Lista lista1){
+	//Endereço de elo que percorrerá a lista
 	struct elo* crawler = lista1.cabec;
+	//Enquanto não chegar no final da lista
 	while(crawler != NULL){
+		//Salva o endereço do atual
 		struct elo* aux = crawler;
+		//Avança o crawler (para desalocar o próximo)
 		crawler = crawler->next;
+		//Limpa o atual
+		//Tem erro aqui **************************** free(): invalid pointer *************** :/
+		if(aux->val != NULL) printf("Estamos livrando o %s\n",aux->val->nome);
 		free(aux->val);
 		free(aux);
 	}
+	printf("livrou uma lista inteira\n");
 	return;
 }
 
 struct elo* Linsere(Lista lista1, Elemento* val){
+	//Percorre a lista até chegar no final (crawler == NULL) ou chegar em alguma posição vazia (crawler->val == NULL)
 	struct elo* crawler = lista1.cabec;
-	//printf("O crawler é nulo?%d\n",crawler = NULL);
 	while(crawler != NULL && crawler->val != NULL){
+		//Achando o último elemento da lista e inserindo mais um
+		if(crawler->next == NULL)
+		{
+			struct elo* proximo = malloc(sizeof(Elo));
+			proximo->val = val;
+			proximo->next = NULL;	
+			crawler->next = proximo;
+			return (crawler->next);
+		
+		}
 		crawler = crawler->next;
 	}
-	printf("O crawler é nulo?%d\n",crawler == NULL);
-	if(crawler == NULL){
-		struct elo* proximo = malloc(sizeof(Elo));
-		proximo->val = val;
-		proximo->next = NULL;
-		crawler = proximo;
-		printf("Chegou no fim da lista :(\nDai inseriu o %s\n", val->nome);
-		//crawler
-		return NULL;
-	}
-	else{
-		crawler->val = val;
-		printf("%s\n",val->nome);
-		return crawler;
-	}
+	
+	//Se achou uma posição sem valor, podemos apenas inserir
+	if(crawler != NULL) crawler->val = val;
+	return crawler;
 }
 
 Elemento* Lbusca(Lista lista1, char* n){
+	//Mesma estrutura do insere, percorre a lista até achar ou chegar no final
 	struct elo* crawler = lista1.cabec;
 	while(crawler != NULL){
-		printf("Busca\n");
-		printf("%s %s\n",crawler->val->nome, n);
-		if(crawler->val != NULL && crawler->val->nome == n){
+		if(crawler->val != NULL && stringsIguais(crawler->val->nome, n)){
 			return crawler;
 		}
 		crawler = crawler->next;
@@ -66,9 +84,12 @@ Elemento* Lbusca(Lista lista1, char* n){
 }
 
 Elemento* Lretira(Lista lista1, Elemento* val){
+	//Percorre a lista até encontrar o elemento
 	struct elo* crawler = lista1.cabec;
 	while(crawler != NULL){
 		if(crawler->val == val){
+			//Apaga o antigo valor e anula o ponteiro
+			free(crawler->val->nome);
 			crawler->val = NULL;
 			return val;
 		}
--- teste.c
+++ teste.c
@@ -1,32 +1,47 @@
 #include "hash.h"
 
 int main(void){
-    TabSim hash = Tcria(4);
     int n;
+    printf("Qual será o tamanho da Tabela de Símbolos? ");
     scanf("%d", &n);
-    printf("oi\n");
-    printf("%d\n", n);
+    TabSim hash = Tcria(n);
+    
+    printf("Quantos elementos deseja inserir? ");
+    scanf("%d", &n);
     Elemento* Elementi = malloc(n*sizeof(Elemento));
     for(int i = 0; i<n; i++){
         char aux[80];
+	printf("Nome do elemento: ");
         scanf("%s", aux);
         int j;
+	//Guardando o nome digitado no Elementi[i]
         for(j = 0; aux[j] != '\0'; j++) Elementi[i].nome[j] = aux[j];
         Elementi[i].nome[j] = '\0';
-        //printf("%d %c\n",i,*aux);
+
+	//Insere o elemento na lista correspondente (posição na tabela de símbolos)
         Tinsere(hash, aux, &Elementi[i]);
     }
-    printf("aaa");
+
     int m;
+    printf("Quantos elementos deseja buscar? ");
     scanf("%d",&m);
     for(int i = 0; i<m; i++){
         char aux[80];
+	printf("Nome do procurado: ");
         scanf("%s", aux);
-
-        if(Tbusca(hash, aux)==NULL) printf("Não achou\n");
-        else printf("Achou\n");
+        if(Tbusca(hash, aux)==NULL) printf("Não achou!\n");
+        else printf("Achou!\n");
     }
+    char retirado[80];
 
-    //Tdestroi(hash);
+    printf("Teste do retira ");
+    scanf("%s", retirado);
+    Tretira(hash, retirado);
+    printf("Busca ai o q voce retirou\n");
+    scanf("%s", retirado);
+    if(Tbusca(hash, retirado) == NULL) printf("Funfou\n");
+    
+
+    Tdestroi(hash);
 
 }
