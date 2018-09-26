1a2a6a3164919843d41d41e4711680596b017e36
pepedrog <43448539+pepedrog@users.noreply.github.com>
2018-09-26 19:04:13 -0300

    Update hash.h
    Só falta consertar o Tdestroi, pus uns comentarios de onde da o erro mas n vi ainda oq era :/

--- hash.h
+++ hash.h
@@ -10,6 +10,10 @@ typedef struct TabSim{
 } TabSim;
 
 int hash(char* s, int mod){
+	//A tabela de hash funcionará transformando a palavra em um número na base 257
+	//E depois tirando o modulo do tamanho da tabela
+
+	//Exemplo: P Ã O retornará (P * 257⁰ + Ã * 257¹ + O * 257²) mod(mod)
 	long long int out = 0;
 	for(int i = 0; s[i] != '\0'; i++){
 		out = out*257 + (int) s[i];
@@ -19,6 +23,7 @@ int hash(char* s, int mod){
 }
 
 TabSim Tcria(int tam){
+	//Criando uma tabela com tam listas
 	TabSim tabela;
 	tabela.listas = malloc(tam*sizeof(Lista));
 	tabela.size = tam;
@@ -29,6 +34,7 @@ TabSim Tcria(int tam){
 }
 
 void Tdestroi(TabSim t){
+	//Percorre a tabela destruindo as listas dela
 	for(int i = 0; i < t.size; i++){
 		Ldestroi(t.listas[i]);
 	}
@@ -37,6 +43,7 @@ void Tdestroi(TabSim t){
 }
 
 int Tinsere(TabSim t, char *n, Elemento *val){
+	//Calcula o hash do elemento, que é em qual lista ele será inserido
 	int hash_val = hash(n, t.size);
 	printf("Hash do elemento: %d\n", hash_val);
 	return Linsere(t.listas[hash_val], val)==NULL?0:1;
@@ -44,15 +51,18 @@ int Tinsere(TabSim t, char *n, Elemento *val){
 
 Elemento* Tbusca(TabSim t, char *n){
 	int hash_val = hash(n, t.size);
+	printf("Estamos buscando o elemento %s que tem hash %d\n", n, hash_val);
 	return Lbusca(t.listas[hash_val], n);
 }
 
 int Tretira(TabSim t, char *n){
 	int hash_val = hash(n, t.size);
-	struct elo* crawler = t.listas[hash_val].cabec; Elemento* out = NULL;
+	struct elo* crawler = t.listas[hash_val].cabec; //Elemento* out = NULL;
 	struct elo* last = crawler;
 	while(crawler != NULL){
-		if(crawler->val != NULL && crawler->val->nome == n){
+		printf("Fazendo uma busca pelo %s para tentar elimina-lo\n", n);
+		if(crawler->val != NULL && stringsIguais(crawler->val->nome, n)){
+			printf("Achei o %s e to eliminando\n", n);
 			last->next = crawler->next;
 			free(crawler->val);
 			free(crawler);
